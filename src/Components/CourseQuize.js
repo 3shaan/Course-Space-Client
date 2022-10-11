05f6b8180fd6e92efe35d83658876666fcd8fe0c
3shaan <3shan.bd@gmail.com>
2022-10-11 20:24:25 +0600

    corse complete

--- src/Components/CourseQuize.js
+++ src/Components/CourseQuize.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizeDetails from './QuizeDetails';
 
 const CourseQuize = () => {
+    const [CountCorrect, setCorrect] = useState(0);
     const quizData = useLoaderData();
     const quizes = quizData.data.questions;
     const {name} = quizData.data;
     // console.log(quizData.data.questions);
+    console.log(CountCorrect);
     return (
         <div>
             <h1 className='text-3xl text-center mt-10'>This is quize section for {name} </h1>
             {
-                quizes.map(quize => <QuizeDetails key={quize.id} quize ={quize} ></QuizeDetails>)
+                quizes.map(quize => <QuizeDetails key={quize.id} quize ={quize} setCorrect={setCorrect} CountCorrect = {CountCorrect} ></QuizeDetails>)
             }
         </div>
     );
--- src/Components/QuizeDetails.js
+++ src/Components/QuizeDetails.js
@@ -3,9 +3,10 @@ import {faCheckSquare} from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const QuizeDetails = ({quize}) => {
+const QuizeDetails = ({quize, setCorrect, CountCorrect}) => {
     const {question, options, correctAnswer} = quize;
     // console.log(quize);
+    
     const correct = (ans)=>{
         // console.log(correctAnswer);
         // console.log(ans);
@@ -14,6 +15,8 @@ const QuizeDetails = ({quize}) => {
             console.log('correct');
             // ansClass.push('bg-blue-900');
             toast("yehh! correct answer");
+            setCorrect( CountCorrect + 1);
+            
 
         }
         else{
@@ -31,8 +34,8 @@ const QuizeDetails = ({quize}) => {
                 <div className='grid grid-cols-2 gap-5'>
                     {options.map(option => {
                        return (
-                        <div onClick={(e)=>correct(e.target.innerText)} key={Math.random()}  className="box-content border text-xl mt-4 rounded-lg h-20 flex items-center justify-center w-96">
-                       <p ><FontAwesomeIcon className='mr-4' icon={faCheckSquare}/> {option}</p>
+                        <div onClick={(e)=>correct(e.target.innerText)} key={Math.random()}  className="box-content border text-xl mt-4 rounded-lg h-20 flex items-center justify-center w-96 p-3 bg-base-200">
+                       <p className='p-4'><FontAwesomeIcon className='mx-4' icon={faCheckSquare}/>{option}</p>
                    </div> 
                        )
                     })}
